153fe09359763aca1637cf37469e46c6da5b9f4d
Patrick Siegler <github@psieg.de>
2016-08-01 16:33:05 +0200

    Rebrand to official

--- Software/src/version.h
+++ Software/src/version.h
@@ -36,11 +36,11 @@
 #define VER_FILEVERSION_STR         VERSION_STR "\0"
 #define VER_PRODUCTVERSION_STR      VER_FILEVERSION_STR
 
-#define VER_PRODUCTNAME_STR         "Prismatik (unofficial)"
-#define VER_COMPANYNAME_STR         "Patrick Siegler"
+#define VER_PRODUCTNAME_STR         "Prismatik"
+#define VER_COMPANYNAME_STR         "Woodenshark LLC"
 #define VER_FILEDESCRIPTION_STR     "Lightpack Software"
 #define VER_INTERNALNAME_STR        "Prismatik"
-#define VER_LEGALCOPYRIGHT_STR      ""
+#define VER_LEGALCOPYRIGHT_STR      "Copyright (c) 2016 Woodenshark LLC"
 #define VER_ORIGINALFILENAME_STR    "Prismatik.exe"
 
 
