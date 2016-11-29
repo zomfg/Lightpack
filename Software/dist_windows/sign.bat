5839e608cace22e6e0befcb5fb4a90cd5000a793
Patrick Siegler <github@psieg.de>
2016-11-29 19:11:35 +0100

    Helper script for signing binaries

---
+++ Software/dist_windows/sign.bat
@@ -0,0 +1,4 @@
+signtool sign /tr http://time.certum.pl/ /n "Woodenshark LLC" content/Prismatik.exe
+signtool sign /tr http://time.certum.pl/ /n "Woodenshark LLC" content/prismatik-*.dll
+signtool sign /tr http://time.certum.pl/ /n "Woodenshark LLC" content/libraryinjector.dll
+signtool sign /tr http://time.certum.pl/ /n "Woodenshark LLC" content/offsetfinder.exe
