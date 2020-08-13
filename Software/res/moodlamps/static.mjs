beb41dc257ebdbb5ff40e7bcfd08055c74f6d26e
zomfg <sergio.kunats@gmail.com>
2020-08-13 21:55:18 +0200

    mood lamps: static lamp: removed note about recommended timing

--- Software/res/moodlamps/static.mjs
+++ Software/res/moodlamps/static.mjs
@@ -1,6 +1,4 @@
 export const name = "Static (default)"
-
-// note: in static or very slow animations it's better to keep the refresh interval under 500ms to avoid bugs
 export const interval = 50 // ms
 
 export function tick(baseColor, colors)
