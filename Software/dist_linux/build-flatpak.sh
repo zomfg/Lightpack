b78e6b636601d9177ffd357ae478f2b0651a9c1e
zomfg <sergio.kunats@gmail.com>
2020-11-08 15:15:16 +0100

    flatpak question mark

---
+++ Software/dist_linux/build-flatpak.sh
@@ -0,0 +1,10 @@
+#!/bin/sh -xe
+distdir=Software/dist_linux
+destdir=/tmp
+rm -rf "$destdir/flatdir" "$destdir/repo"
+flatpak install --assumeyes org.kde.Sdk//5.15 org.kde.Platform//5.15
+flatpak-builder --delete-build-dirs --repo="$destdir/repo" "$destdir/flatdir" "$distdir/de.psieg.Prismatik.yml"
+# flatpak-builder --user --install "$destdir/flatinstall" "$distdir/de.psieg.Prismatik.yml"
+# flatpak run de.psieg.Prismatik
+# flatpak build-export "$destdir/repo" "$destdir/flatdir"
+flatpak build-bundle "$destdir/repo" prismatik.flatpak de.psieg.Prismatik
