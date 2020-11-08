5c11dc42727a8e567daf00542cd7fc2dc68f5281
zomfg <sergio.kunats@gmail.com>
2020-11-08 22:04:59 +0100

    flatpak n gitignore

--- Software/dist_linux/build-flatpak.sh
+++ Software/dist_linux/build-flatpak.sh
@@ -1,10 +1,14 @@
 #!/bin/sh -xe
-distdir=Software/dist_linux
+kde_version=5.15
+flatpak_id=de.psieg.Prismatik
 destdir=/tmp
+VERSION=`cat ../VERSION`
+
+[ ! -e "$flatpak_id.yml" ] && echo "manifest $flatpak_id.yml not found" && exit 1
+
 rm -rf "$destdir/flatdir" "$destdir/repo"
-flatpak install --user --assumeyes org.kde.Sdk//5.15 org.kde.Platform//5.15
-flatpak-builder --delete-build-dirs --repo="$destdir/repo" "$destdir/flatdir" "$distdir/de.psieg.Prismatik.yml"
-# flatpak-builder --user --install "$destdir/flatinstall" "$distdir/de.psieg.Prismatik.yml"
-# flatpak run de.psieg.Prismatik
+flatpak install --user --assumeyes org.kde.Sdk//"$kde_version" org.kde.Platform//"$kde_version"
+flatpak-builder --delete-build-dirs --repo="$destdir/repo" "$destdir/flatdir" "$flatpak_id.yml"
 # flatpak build-export "$destdir/repo" "$destdir/flatdir"
-flatpak build-bundle "$destdir/repo" prismatik.flatpak de.psieg.Prismatik
+flatpak build-bundle "$destdir/repo" "prismatik_$VERSION.flatpak" "$flatpak_id"
+# flatpak run de.psieg.Prismatik
