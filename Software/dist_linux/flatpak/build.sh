0434f8ae8d2abe6214c2ff26bd168d63ec06cd96
zomfg <sergio.kunats@gmail.com>
2020-11-11 21:41:10 +0100

    dpkg/pacman/flatpak test

--- Software/dist_linux/flatpak/build.sh
+++ Software/dist_linux/flatpak/build.sh
@@ -9,12 +9,12 @@ set -xe
 sed '
 	s#__APP_ID__#'"$flatpak_id"'#
 	s#__KDE_VERSION__#'"$kde_version"'#
-	' "$flatpak_id.yml.template" > "$flatpak_id.yml"
+	' manifest.yml.template > "$flatpak_id.yml"
 
 [ ! -e "$flatpak_id.yml" ] && echo "manifest $flatpak_id.yml not found" && exit 1
 
 rm -rf "$destdir/flatdir" "$destdir/repo"
-flatpak remote-add --user --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+# flatpak remote-add --user --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak install --user --assumeyes org.kde.Sdk//"$kde_version" org.kde.Platform//"$kde_version"
 flatpak-builder --delete-build-dirs --repo="$destdir/repo" "$destdir/flatdir" "$flatpak_id.yml"
 # flatpak build-export "$destdir/repo" "$destdir/flatdir"
